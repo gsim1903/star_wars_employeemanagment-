be810f1b43510ce220e1846dd2939db9904b5851
gsim1903 <gavin@internetbusker.co>
2022-05-16 14:15:06 +0200

    adds axios and use state to emplployees use component

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 //get
 //fetch it with axios
+
+
 //store the data
 //rely on useState (componetstate)
 //dsiplay the jsx return
 // use.map to create an aray with jsx
 
 const EmployeeList = () => {
-  return (
+    const [employees, setEmployees] = uEeState([])
+  
+    const fetchEmployees = () => {
+        const response = axios.get
+    }
+  
+    return (
     <div>EmployeeList</div>
   )
 }
